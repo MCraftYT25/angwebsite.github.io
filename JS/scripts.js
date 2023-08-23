ce518b72ce8e04918339a92c9e760ff4d2195ee9
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2023-08-23 14:37:37 +0200

    Add files via upload
    1.7

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,5 +4,21 @@ let cookies = document.cookie.split(/; /g);
 cookies = String(cookies);
 let version = cookies.slice(8,11);*/
 
-const version = 1.6; 
+const version = 1.7; 
 document.getElementById('version').innerText = "V" + version;
+
+let x = 0;
+    function chgcolor()
+    {
+        if(x == 0)
+        {
+            document.body.style.setProperty("--main-color", "#333");
+            document.body.style.setProperty("--second-color", "rgb(253,217,141)");
+            x = x + 1;
+        } else {
+            document.body.style.setProperty("--main-color", "rgb(253,217,141)");
+            document.body.style.setProperty("--second-color", "black");
+            x = x - 1;
+        }
+        
+    }
