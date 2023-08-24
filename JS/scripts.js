cb411a3aa545684aae796b7086a6150efdec5d0a
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2023-08-24 13:08:01 +0200

    Add files via upload

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,23 @@
-/*document.cookie = "version=1.6";
-
-let cookies = document.cookie.split(/; /g);
-cookies = String(cookies);
-let version = cookies.slice(8,11);*/
-
-const version = 1.7; 
+const version = 1.8; 
 document.getElementById('version').innerText = "V" + version;
 
 let x = 0;
-    function chgcolor()
+
+function chgcolor()
+{
+    if(x == 0)
     {
-        if(x == 0)
-        {
-            document.body.style.setProperty("--main-color", "#333");
-            document.body.style.setProperty("--second-color", "rgb(253,217,141)");
-            x = x + 1;
-        } else {
-            document.body.style.setProperty("--main-color", "rgb(253,217,141)");
-            document.body.style.setProperty("--second-color", "black");
-            x = x - 1;
-        }
-        
-    }
+        document.body.style.setProperty("--main-color", "#333");
+        document.body.style.setProperty("--second-color", "rgb(253,217,141)");
+        document.body.style.setProperty("--fourth-color", "rgb(253,217,141)");
+        document.body.style.setProperty("--fifth-color", "#333");
+        x = x + 1;
+    } else {
+        document.body.style.setProperty("--main-color", "rgb(253,217,141)");
+        document.body.style.setProperty("--second-color", "black");
+        document.body.style.setProperty("--fourth-color", "#f2a86f");
+        document.body.style.setProperty("--fifth-color", "white");
+            
+        x = x - 1;
+    }      
+}
